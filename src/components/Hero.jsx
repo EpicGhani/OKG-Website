e5f1dfb187f2761bc611d3e52ae42d57962dd8d5
Al-Ghani Abdulla <al_ghani12@yahoo.com>
2025-12-25 18:34:31 +0800

    updated hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero({hasWorks}) {
       <div className="heroBg" aria-hidden="true"/>
       <div className="sectionInner">
         <h1 className="heroTitle">Games are our Obra.<br/><span className="accent">Made in PH.</span></h1>
-        <p className="muted heroText">We're a small team of passionate devs crafting unique worlds, one pixel at a time.</p>
+        <p className="heroText">We're a small team of passionate devs crafting unique worlds, one pixel at a time.</p>
         <div className="btnRow">
           <a className="btn heroBtn" href="#contact">Work with us</a>
           {hasWorks && <a className="btnOutline heroOutline" href="#works">See our work</a>}
